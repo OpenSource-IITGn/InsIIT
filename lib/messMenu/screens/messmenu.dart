8d9d1e59c2019d827c8eb5e079fccc2f40cf8ce3
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2020-12-24 18:16:52 +0530

    Update messmenu.dart

--- lib/messMenu/screens/messmenu.dart
+++ lib/messMenu/screens/messmenu.dart
@@ -3,7 +3,7 @@ import 'package:csv/csv.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
-import 'file:///D:/Gaurav/Github_copy/Instiapp_Dev/IITGN-Institute-App/lib/messMenu/classes/weekdaycard.dart';
+import 'package:instiapp/messMenu/classes/weekdaycard.dart';
 import 'package:instiapp/mainScreens/homePage.dart';
 import 'package:instiapp/utilities/constants.dart';
 import 'package:instiapp/utilities/globalFunctions.dart';
