fa5fd7f0a58e3b9c8afcd4550b9ea49d754804f1
awesomeroks <30774147+awesomeroks@users.noreply.github.com>
2021-02-05 10:23:27 +0530

    added github launch thing

--- lib/developers/screens/developers.dart
+++ lib/developers/screens/developers.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 
@@ -101,6 +102,12 @@ class _DevelopersPageState extends State<DevelopersPage> {
           },
         ),
         actions: [
+          IconButton(
+            icon: FaIcon(FontAwesomeIcons.github),
+            onPressed: () {
+              launch('https://github.com/praveenVnktsh/IITGN-Institute-App');
+            },
+          ),
           IconButton(
               icon: Icon(
                 Icons.refresh,
