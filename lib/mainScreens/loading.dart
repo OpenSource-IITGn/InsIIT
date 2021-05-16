0c76dc986afadda943f345bb1ebde3b20e53fea3
awesomeroks <30774147+praveenVnktsh@users.noreply.github.com>
2021-05-16 12:20:53 +0530

    Changed loading screen

--- lib/mainScreens/loading.dart
+++ lib/mainScreens/loading.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:instiapp/utilities/constants.dart';
 
 Widget loadScreen() {
   return Scaffold(
     body: Container(
       color: Color.fromARGB(255, 255, 137, 87),
       child: Center(
-          child: Text(
-        "Please wait...",
-        style: TextStyle(color: Colors.white, fontSize: 20),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Container(
+          //     width: 200, child: Image.asset('assets/images/insiiticon.png')),
+          Text(
+            "InsIIT",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 80),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "A HackRush Project",
+            style: TextStyle(
+                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20),
+          ),
+          SizedBox(height: 40),
+          Text(
+            "Please wait ... ",
+            style: TextStyle(
+                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 20),
+          ),
+          // SizedBox(height: 10),
+          // Container(
+          //     width: 200,
+          //     child: Image.asset(
+          //       'assets/images/hackrush.png',
+          //     )),
+        ],
       )),
     ),
   );
